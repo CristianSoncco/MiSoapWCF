b822b715d09fd1d9365b9078cce3e36d7a45d979
Cristian Soncco-CSTI <cristian.soncco-CSTI@quimicasuiza.com>
2022-10-30 10:19:33 -0500

    Agregar archivos de proyecto.

---
+++ Cliente/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mi.Service1Client oClient = new Mi.Service1Client();
+            string res = oClient.GetData(5,3);
+            MessageBox.Show(res);
+
+            Mi.CompositeType oData = new Mi.CompositeType();
+            oData.BoolValue = true;
+            var res2= oClient.GetDataUsingDataContract(oData);
+            MessageBox.Show(res2.StringValue);
+        }
+    }
+}
---
+++ MiSoap/Service1.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MiSoap
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value, int value2)
+        {
+            return string.Format("tu Suma: {0}", (value+value2));
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Tiene valor verdadero.";
+            }
+            return composite;
+        }
+    }
+}
